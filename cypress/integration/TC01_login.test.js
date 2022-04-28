efd4440cddde4167ce7d196d51fe5350af2e3835
Dika Brenda <dika.brenda@paques.id>
2022-04-28 16:31:50 +0700

    update scenario case login

--- cypress/integration/TC01_login.test.js
+++ cypress/integration/TC01_login.test.js
@@ -1,21 +1,13 @@
 import loginPage from "../pages/login.page";
 import login_data from "../fixtures/login.data";
 
-
 describe('as a user, i want to login successfully', () => {
 
     login_data.forEach(data => {
-        describe(data.name, () => {
-            it('user filled in username', () => {
-                loginPage.setUsername(data.username)
-            })
-            it('user filled in password', () => {
-                loginPage.setPassword(data.password)
-            })
-
-            it('user tap login button', () => {
-                loginPage.clickLogin()
-            })
+        it(data.name, () => {
+            loginPage.setUsername(data.username)
+            loginPage.setPassword(data.password)
+            loginPage.clickLogin()
         })
     })
 })
--- cypress/integration/TC02_FailedCase.test.js
+++ cypress/integration/TC02_FailedCase.test.js
@@ -2,18 +2,10 @@ import loginPage from "../pages/login.page";
 
 describe('this for scenario failed case and uploaded image', () =>{
 
-    describe('Wording Message Invalid Login', () => {
-        it('user filled in username', () => {
-            loginPage.setUsername('Admin')
-        })
-        it('user filled in wrong password', () => {
-            loginPage.setPassword('!@£!$@£')
-        })
-        it('user tap login button', () => {
-            loginPage.clickLogin()
-        })
-        it('user verify a wording message', () => {
-            loginPage.wordingMessage()
-        })
+    it('User Login using Invalid Password', () => {
+        loginPage.setUsername('Admin')
+        loginPage.setPassword('!@£!$@£')
+        loginPage.clickLogin()
+        loginPage.wordingMessage()
     })
 })
