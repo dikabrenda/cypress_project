407ba6ee8490d0dd757aa705ae94d75cfec4769a
Dika Brenda <dika.brenda@paques.id>
2022-05-05 21:06:58 +0700

    index .js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -19,19 +19,30 @@ import dotenv, {config} from "dotenv"
 import loginPage from '../pages/login.page';
 dotenv.config()
 
-before(() => {
-  console.log("Navigate Browser");
-  loginPage.openBrowser()
-})
+// before(() => {
+  // console.log("Navigate Browser");
+  // loginPage.openBrowser()
+// })
 
-beforeEach(() => {
-  console.log("Login");
-})
+// beforeEach(() => {
+  // console.log("Login");
+// })
+// 
+// afterEach(() => {
+  // console.log("Logout");
+// })
 
-afterEach(() => {
-  console.log("Logout");
-})
+// after(() => {
+  // console.log("Close Browser");
+// })
 
-after(() => {
-  console.log("Close Browser");
-})
+
+export default (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  config.env.credentials = {
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+  }
+  return config
+}
